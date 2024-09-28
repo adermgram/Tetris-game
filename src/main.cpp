e240809fefe816f0b614c8441994bd3f715cd299
Hans-Peter Beständig <hdusel@tangerine-soft.de>
2024-09-28 03:38:32 +0200

    changed: Get rid of new and allocate the object on the stack.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
 #include "tetris.h"
-#include <new>
 #include <cstdlib>
 
 int main(int argc, char* argv[])
 {
     srandom(time(nullptr));
-    Tetris* game = new(std::nothrow) Tetris(argc, argv);
-    if (game)
-    {
-        game->run();
-    }
+    Tetris game(argc, argv);
+    game.run();
     return EXIT_FAILURE;
 }
